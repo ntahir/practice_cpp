3c078c24623cd5cae91921bf12d1e4c2e733c0a5
Nauman <nauman.tahir@gmail.com>
2014-07-29 15:49:32 -0700

    c++ practice code

---
+++ test_main.cpp
@@ -0,0 +1,17 @@
+/*
+ * test_main.cpp
+ *
+ *  Created on: Jul 26, 2014
+ *      Author: nauman
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "this is test c++ program from eclipse" << endl;
+	return 0;
+}
+
+
